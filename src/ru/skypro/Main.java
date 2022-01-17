0dfae91dc57cb64d34cd39754bd4f8b3316e3780
Антон <antonL1790@yandex.ru>
2022-01-17 22:50:19 +0300

    ready

--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -8,5 +8,31 @@ public class Main {
 
     }
 
+    public static void task1() {
 
+        for (int i = 0; i <= 30; i++) {
+            if (i % 3 == 0) {
+                System.out.println(i + ": ping");
+            } else if (i % 5 == 0) {
+                System.out.println(i + ": pong");
+            } else if (i % 3 == 0 && i % 5 == 0) {
+                System.out.println(i + ": ping pong");
+            } else System.out.println(i + ":");
+        }
+    }
+
+    public static void task2() {
+        int a = 0;
+        int b = 1;
+        int nextNumber;
+        System.out.println(a);
+        System.out.println(b);
+        for (int i = 2; i <= 9; i++) {
+
+            nextNumber = a + b;
+            System.out.println(nextNumber + " ");
+            a = b;
+            b = nextNumber;
+        }
+    }
 }

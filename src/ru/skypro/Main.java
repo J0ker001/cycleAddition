5034d2305f5232126d07f436667baf101604c2f5
Антон <antonL1790@yandex.ru>
2022-01-18 21:48:52 +0300

    Первую переделал, понял что не работало на 15 и 30. А вот со второй, не понял. ( как за один цикл, 2 числа вытаскивать)

--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -6,17 +6,18 @@ public class Main {
         task1();
         task2();
 
+
     }
 
     public static void task1() {
 
-        for (int i = 0; i <= 30; i++) {
-            if (i % 3 == 0) {
+        for (int i = 1; i <= 30; i++) {
+            if (i % 3 == 0 & i % 5 == 0) {
+                System.out.println(i + ": ping pong ");
+            } else if (i % 3 == 0) {
                 System.out.println(i + ": ping");
             } else if (i % 5 == 0) {
                 System.out.println(i + ": pong");
-            } else if (i % 3 == 0 && i % 5 == 0) {
-                System.out.println(i + ": ping pong");
             } else System.out.println(i + ":");
         }
     }
@@ -25,10 +26,8 @@ public class Main {
         int a = 0;
         int b = 1;
         int nextNumber;
-        System.out.println(a);
-        System.out.println(b);
+        System.out.println(a + "" + "\n" + b + "");
         for (int i = 2; i <= 9; i++) {
-
             nextNumber = a + b;
             System.out.println(nextNumber + " ");
             a = b;
